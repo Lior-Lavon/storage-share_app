692150d8d6dff60123eab3b41070f1a13084f0fa
Lior Lavon <liorlavon554@gmail.com>
2025-06-12 13:58:14 +0200

    adding tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
-    <div>
-      <h1 className="bg-red-500">Hello world</h1>
+    <div className="w-full h-[100vh] flex items-center justify-center">
+      <h1 className=" text-2xl">StorageShare App</h1>
     </div>
   );
 }
