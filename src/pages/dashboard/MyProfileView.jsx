a332a540217d45a641236b510f55c84b6b4757ef
Lior Lavon <liorlavon554@gmail.com>
2025-06-18 18:50:10 +0200

    1

--- src/pages/dashboard/MyProfileView.jsx
+++ src/pages/dashboard/MyProfileView.jsx
@@ -176,7 +176,7 @@ const MyProfileView = ({ isVisible }) => {
 
                 <PrimaryButton type="submit">Submit</PrimaryButton>
               </form>
-              <div className="w-full h-10"></div>
+              <div className="w-full h-16 bg-amber-500"></div>
             </div>
           </div>
         </div>
