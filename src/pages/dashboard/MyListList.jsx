7dda0945135ef5e63de5e0f0e39882e1229e9a85
Lior Lavon <liorlavon554@gmail.com>
2025-06-24 13:24:12 +0200

    1

--- src/pages/dashboard/MyListList.jsx
+++ src/pages/dashboard/MyListList.jsx
@@ -1,15 +1,26 @@
 import React from "react";
 import { ShortListing } from "../../components";
+import PrimaryButton from "../../components/SharedComponents/PrimaryButton";
 
 const MyListList = () => {
   return (
-    <div className="w-full">
-      <div className="w-full py-2 flex flex-col gap-4">
+    <div className="w-full h-full flex flex-col">
+      {/* Scrollable list */}
+      <div className="flex-1 overflow-y-auto py-2 flex flex-col gap-4">
         <ShortListing />
         <ShortListing />
         <ShortListing />
         <ShortListing />
         <ShortListing />
+      </div>
+
+      {/* Fixed button at the bottom */}
+      <div className="bg-white flex items-center justify-center">
+        <div className="w-full py-1">
+          <PrimaryButton onClick={() => inputRef.current.click()}>
+            Create new listing
+          </PrimaryButton>
+        </div>
       </div>
     </div>
   );
