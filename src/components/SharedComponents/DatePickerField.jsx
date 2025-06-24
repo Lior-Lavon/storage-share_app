513247782bca907455e05b84a030ee53538601e7
Lior Lavon <liorlavon554@gmail.com>
2025-06-24 17:27:51 +0200

    1

--- src/components/SharedComponents/DatePickerField.jsx
+++ src/components/SharedComponents/DatePickerField.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import DatePicker from "react-datepicker";
 import { parseISO } from "date-fns";
 import "react-datepicker/dist/react-datepicker.css";
+import { FaRegCalendarAlt } from "react-icons/fa";
 
 const DatePickerField = ({
   label,
@@ -32,17 +33,23 @@ const DatePickerField = ({
   return (
     <div className={`flex flex-col gap-1 ${className}`}>
       {label && <label className="text-base font-medium">{label}</label>}
-      <DatePicker
-        selected={selectedDate}
-        onChange={handleChange}
-        minDate={minDate}
-        maxDate={maxDate}
-        disabled={disabled}
-        placeholderText="dd/mm/yyyy"
-        dateFormat="dd/MM/yyyy"
-        className="w-full bg-gray-100 px-2 py-1 rounded border border-gray-300 focus:outline-none focus:ring-2 focus:ring-violet-500"
-        name={name}
-      />
+      <div className="relative">
+        <DatePicker
+          selected={selectedDate}
+          onChange={handleChange}
+          minDate={minDate}
+          maxDate={maxDate}
+          disabled={disabled}
+          placeholderText="dd/mm/yyyy"
+          dateFormat="dd/MM/yyyy"
+          className="w-full bg-gray-100 px-2 py-1 rounded border border-gray-100 focus:outline-none focus:ring-2 focus:ring-violet-500"
+          name={name}
+        />
+        <FaRegCalendarAlt
+          className="absolute right-8 top-1/2 transform -translate-y-1/2 text-gray-300 pointer-events-none"
+          size={18}
+        />
+      </div>
     </div>
   );
 };
